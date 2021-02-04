15a05fdef3818c6db64f3a1522e595e44712efb6
aunal16 <aunal16@ku.edu.tr>
2021-02-04 16:59:19 +0300

    circle and points are plotted for N=10

--- probabilistic_pi.py
+++ probabilistic_pi.py
@@ -1,6 +1,7 @@
 # IMPORT LIBRARIES
 from random import seed
 from random import random
+import matplotlib.pyplot as plt
 
 # SET SEED (random)
 seed(13)
@@ -9,6 +10,10 @@ seed(13)
 N = 10
 pairs = list()
 
+fig, ax = plt.subplots(figsize=(4,4))
+marker_size = 5
+
+# DEFINE FUNCTIONS
 def generate_points(num_of_points):
     n = num_of_points
     
@@ -18,7 +23,15 @@ def generate_points(num_of_points):
 
         pairs.append((x,y))
 
+def plot_circle():
+    circle = plt.Circle((0.5,0.5), 0.5, color = 'g', fill = False)
+    ax.add_patch(circle)
+
 if __name__ == "__main__":
     generate_points(N)
-    print(pairs)
-
+    plot_circle()
+    plt.scatter(*zip(*pairs), s = marker_size)
+    
+    plt.xlabel("x");    plt.ylabel("y");
+    plt.xlim((0,1));    plt.ylim((0,1));
+    plt.show()
