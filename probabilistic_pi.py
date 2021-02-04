f24d70c13f183efb0623b4de7bd59dced4cd69b0
aunal16 <aunal16@ku.edu.tr>
2021-02-04 19:28:16 +0300

    approximated value of pi is calculated and printed with real pi, error is printed, radius is made changeable and marker size is reduces for visibility on large valus of N

--- probabilistic_pi.py
+++ probabilistic_pi.py
@@ -8,26 +8,21 @@ import math
 seed(13)
 
 # SET GLOBAL VARIABLES
-N = 10
-pairs = list()
-distances = list()
-
-fig, ax = plt.subplots(figsize=(4,4))
-marker_size = 5
+N = 10**5
+radius = 0.5
+origin = [0.5, 0.5]
+pairs, distances = list(), list()
+marker_size = 1
 
 # DEFINE FUNCTIONS
 def generate_points(num_of_points):
-    
-    n = num_of_points
-    
-    for _ in range(N):
+    for _ in range(num_of_points):
         x = random()
         y = random()
-
         pairs.append((x,y))
 
 def plot_circle():
-    circle = plt.Circle((0.5,0.5), 0.5, color = 'g', fill = False)
+    circle = plt.Circle((0.5,0.5), radius, color = 'g', fill = False)
     ax.add_patch(circle)
 
 def calc_distances():
@@ -36,16 +31,24 @@ def calc_distances():
         distances.append(dist)
 
 if __name__ == "__main__":
+
+    fig, ax = plt.subplots(figsize=(4,4))
     generate_points(N)
     plot_circle()
     plt.scatter(*zip(*pairs), s = marker_size)
 
     calc_distances()
 
-    outside = sum(map(lambda x : x > 0.5, distances))
+    outside = sum(map(lambda x : x > radius, distances))
     inside = N - outside
 
     print("Inside:", inside, "Outside:", outside)
+    ratio = inside / N
+    pi = ratio * 4
+    pi_str = '{0:.15g}'.format(ratio*4)
+    print("Monte Carlo Integration Result: \u03c0 =", pi_str)
+    print("Exact value of \u03c0 =", math.pi)
+    print("Error:", (pi - math.pi)/math.pi*100,"%")
     
     plt.xlabel("x");    plt.ylabel("y");
     plt.xlim((0,1));    plt.ylim((0,1));
